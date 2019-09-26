a7f4f50c5cc4e0b298d0d2073cb2aca86ff8619d
jake hallam <jakehallam95@gmail.com>
2019-09-26 19:03:35 +0100

    It's a start, i guess#

--- docs/todo.md
+++ docs/todo.md
@@ -1,6 +1,19 @@
 # ToDo!
 
+
 ## List
+### Tests
+* There needs to be a:
+  * shared file
+  * that contains a quick and easy way
+  * to setup a database
+  * with customisable data
+  * from another file?
+
+* data could literally just be a fucking json, that i load in
+* how about some sort of "Build data from json" shit, that sounds cash.
+
+###Exercises
 * This is just going to be focused on selection of data
   * SELECT
   * COUNT
--- test/1.select.test.js
+++ test/1.select.test.js
@@ -1,36 +1,48 @@
 import DEF from "../index.js";
 const sqlite3 = require('sqlite3').verbose();
- 
-async function setup( db ){
-  await new Promise(( resolve, reject ) => {
-    db.serialize(() => {
-      db.run("CREATE TABLE lorem (info TEXT)");
+
+async function dbFromJson( json ){
+  const db = new sqlite3.Database(":memory:"); 
+  
+  db.serialize(() => {
+    const tables = Object.keys( json );
+
+    for( var i = 0; i < tables.length; ++i ){
+      const name = tables[0];
+      //db.run("CREATE TABLE lorem (info TEXT)");
+      const createQuery = `CREATE TABLE ${name} lorem;`
+      //db.run( createQuery );
+      const columns = json[name].columns;
+      const columnQuery = columns.reduce( ( a, x ) => `${a}, ${x}`);
+      console.log( columnQuery );
+
+      const tableData = json[name].rows.reduce( ( a, x ) => `${a}, ${x}` );
+      console.log( tableData );
+    }
+
+
+
+
+/*
+
       var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
       for (var i = 0; i < 10; i++) {
         stmt.run("Ipsum " + i);
       }
+
       stmt.finalize();
       resolve();
-    });
+      */
+
   });
+
+  return db;
 }
 
 describe("This is an example", () => {
   it("This is a test!", async () => {
     const { balls } = DEF;
-    console.log( balls );
-
-    const db = new sqlite3.Database(":memory:");
-    await setup( db );
-    
-    const result = await new Promise(( resolve, reject ) => {
-      db.all("SELECT * from lorem", ( err, rows ) => {
-        resolve(rows);
-      });
-    });
-
-    console.log( result );
-
+    const db = await dbFromJson( require("./databases/example.json") );
     db.close();
   });
 });
